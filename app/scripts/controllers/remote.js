32699a036c79df2d25b5189106a3ccfd7e8525cb
Gabriel de Tassigny <gabriel@multivac.cc>
2014-11-08 14:10:54 +0100

    Added simpler input methods + added Home and Back buttons

--- app/scripts/controllers/remote.js
+++ app/scripts/controllers/remote.js
@@ -3,16 +3,7 @@
 'use strict';
 
 app.controller('RemoteCtrl', ['$scope', 'KodiRemote', function ($scope, KodiRemote) {
-    $scope.upButton = function() {
-      KodiRemote.up();
-    };
-    $scope.downButton = function() {
-      KodiRemote.down();
-    };
-    $scope.rightButton = function() {
-      KodiRemote.right();
-    };
-    $scope.leftButton = function() {
-      KodiRemote.left();
+    $scope.inputButton = function(action) {
+      KodiRemote.input(action);
     };
   }]);
--- app/scripts/services/remote.js
+++ app/scripts/services/remote.js
@@ -5,18 +5,9 @@
 services.service('KodiRemote', ['KodiWS',
   function(KodiWS) {
     var remote = {
-      left: function() {
-        KodiWS.send('Input.Left');
+      input: function(action) {
+        KodiWS.send('Input.' + action);
       },
-      right: function() {
-        KodiWS.send('Input.Right');
-      },
-      down: function() {
-        KodiWS.send('Input.Down');
-      },
-      up: function() {
-        KodiWS.send('Input.Up');
-      }
     };
     return remote;
   }
