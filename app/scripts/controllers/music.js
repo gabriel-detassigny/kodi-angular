2110121db16deabe313d949e5f6c8cb079476411
Gabriel de Tassigny <gabriel@multivac.cc>
2015-01-06 22:20:06 +0000

    Display all artists and their corresponding genre

--- app/scripts/controllers/music.js
+++ app/scripts/controllers/music.js
@@ -3,5 +3,7 @@
 'use strict';
 
 app.controller('MusicCtrl', ['$scope', 'Music', function($scope, Music) {
-  
+  Music.artists().then(function(data) {
+    $scope.artists = data;
+  });
 }]);
--- app/scripts/services/music.js
+++ app/scripts/services/music.js
@@ -4,7 +4,13 @@
 
 services.service('Music', ['KodiWS', '$q', function(KodiWS, $q) {
   var music = {
-
+    artists: function() {
+      var deferred = $q.defer();
+      KodiWS.send('AudioLibrary.GetArtists', { properties: ['genre'] }).then(function(data) {
+        deferred.resolve(data.artists);
+      });
+      return deferred.promise;
+    },
   };
   return music;
 }]);
