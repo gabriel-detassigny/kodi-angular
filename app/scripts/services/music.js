53e398c8964f479f400f7297f5888d9d71592a1e
Gabriel de Tassigny <gabriel@multivac.cc>
2015-03-22 20:46:33 +0000

    Sort artists

--- app/scripts/services/music.js
+++ app/scripts/services/music.js
@@ -6,7 +6,7 @@ services.service('Music', ['KodiWS', '$q', function(KodiWS, $q) {
   var music = {
     artists: function() {
       var deferred = $q.defer();
-      KodiWS.send('AudioLibrary.GetArtists', { properties: ['genre'] }).then(function(data) {
+      KodiWS.send('AudioLibrary.GetArtists', { properties: ['genre'], sort: { method: 'artist' }}).then(function(data) {
         deferred.resolve(data.artists);
       });
       return deferred.promise;
