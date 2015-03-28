913d9fc8269860edd404f6749b56bc7d54ad16ea
Gabriel de Tassigny <gabriel@multivac.cc>
2015-03-28 17:14:18 +0000

    Duration format

--- app/scripts/services/music.js
+++ app/scripts/services/music.js
@@ -29,6 +29,11 @@ services.service('Music', ['KodiWS', '$q', function(KodiWS, $q) {
         result.artist = data.albumdetails.artist[0];
         KodiWS.send('AudioLibrary.GetSongs', { properties: ['title', 'track', 'duration'], sort: { method: 'track' }, filter: { albumid: parseInt(albumId) }}).then(function(data) {
           result.songs = data.songs;
+          for (var i = 0; i < result.songs.length; i++)
+          {
+            result.songs[i].minutes = Math.floor(result.songs[i].duration / 60);
+            result.songs[i].seconds = Math.floor(result.songs[i].duration % 60);
+          }
           deferred.resolve(result);
         });
       });
