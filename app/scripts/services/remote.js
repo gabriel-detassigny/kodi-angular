1d55166c42e057297a29ead093aefe76e3711393
Gabriel de Tassigny <gabriel@multivac.cc>
2015-06-14 23:18:02 +0100

    Comments on remote service

--- app/scripts/services/remote.js
+++ app/scripts/services/remote.js
@@ -6,9 +6,22 @@ services.service('KodiRemote', ['KodiWS',
   function(KodiWS) {
 
     var remote = {
+
+      /**
+       * Send a basic input action to Kodi (Up, Down, Back, etc)
+       *
+       * @param string action : the name of the action.
+       * @return void
+       */
       input: function(action) {
         KodiWS.send('Input.' + action, {});
       },
+
+      /**
+       * Set the volume down.
+       *
+       * @return void
+       */
       volumeDown: function() {
         KodiWS.send('Application.GetProperties', {properties: ['volume']}).then(function(data) {
           if (data.volume > 0)
@@ -17,6 +30,12 @@ services.service('KodiRemote', ['KodiWS',
             }
         });
       },
+
+      /**
+       * Set the volume up.
+       *
+       * @return void
+       */
       volumeUp: function() {
         KodiWS.send('Application.GetProperties', {properties: ['volume']}).then(function(data) {
           if (data.volume < 100)
