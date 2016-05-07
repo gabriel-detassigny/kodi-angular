ff0398cfe04ad24946041f2152d6e93d1f4ca069
Gabriel de Tassigny <gabriel@multivac.cc>
2016-05-07 22:37:26 +0100

    Font awesome builded by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -323,6 +323,11 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'bower_components/font-awesome',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
